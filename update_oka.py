27c9c93f98ad18518460a7406df6431d4a756747
Tomoyuki UBE <ube@coder.okinawa.jp>
2022-01-12 22:15:23 +0900

    add new format

--- update_oka.py
+++ update_oka.py
@@ -45,6 +45,71 @@ def convertKanjiDateTime2En(kanji_datetime):
     en_datetime = tdatetime.strftime('%Y-%m-%d %H:%M:%S')
     return en_datetime
 
+def get_pdf_typeA(file):
+    resize_pdf_oka.resize(file, './pdf/resize_oka.pdf')
+    dummy_line_oka.output_dummy_TypeA('./component/dummy_line_oka.pdf')
+    dummy_line_oka.output_mergePDF('./component/dummy_line_oka.pdf',
+                                './pdf/resize_oka.pdf', './pdf/processed_latest_oka.pdf')
+    pdf = pdfplumber.open('./pdf/processed_latest_oka.pdf')
+    return pdf
+
+def get_pdf_typeB(file):
+    resize_pdf_oka.resize(file, './pdf/resize_oka.pdf')
+    dummy_line_oka.output_dummy_TypeB('./component/dummy_line_oka.pdf')
+    dummy_line_oka.output_mergePDF('./component/dummy_line_oka.pdf',
+                                './pdf/resize_oka.pdf', './pdf/processed_latest_oka.pdf')
+    pdf = pdfplumber.open('./pdf/processed_latest_oka.pdf')
+    return pdf
+
+def pdf_to_data(pdf):
+    for page in pdf.pages:
+        bounding_box = (250, 74, 400, 90)
+        page_crop = page.within_bbox(bounding_box)
+        page_crop.to_image(resolution=200).save(
+            "./snapshot/lastupdate_oka.png", format="PNG")
+        writedata['lastupdate'] = convertKanjiDateTime2En(page_crop.extract_text())
+        tables = page.extract_tables({
+            "vertical_strategy": "lines",
+            "horizontal_strategy": "lines",
+            "intersection_y_tolerance": 1,
+            "min_words_horizontal": 2,
+        })
+        for table in tables:
+            localDf = pd.DataFrame(table)
+            pos = -1
+            for index, row in localDf.iterrows():
+                if row[0] != None and row[0].find('入院中') != -1:
+                    pos = 0
+                    break
+                elif row[1] != None and row[1].find('入院中') != -1:
+                    pos = 1
+                    break
+            if pos == -1:
+                break
+            for index, row in localDf.iterrows():
+                if row[pos] == None:
+                    print('none')
+                elif len(row[pos]) == 0:
+                    if (index == 2 or index == 3) and row[pos+2] != None and row[pos+2].find('中等') != -1:
+                        writedata['moderate'] = int(row[pos+3])
+                elif row[pos].find('入院中') != -1:
+                    writedata['hospitalize'] = int(row[pos+1])
+                    writedata['severe'] = int(row[pos+3])
+                elif row[pos].find('調整中') != -1:
+                    writedata['wait'] = int(row[pos+1])
+                elif row[pos].find('宿泊') != -1:
+                    writedata['hotel'] = int(row[pos+1])
+                elif row[pos].find('自宅') != -1:
+                    writedata['home'] = int(row[pos+1])
+                elif row[pos].find('解除確認') != -1:
+                    writedata['checkout'] = int(row[pos+1])
+                elif row[pos].find('勧告解除') != -1:
+                    writedata['release'] = int(row[pos+1])
+                elif row[pos].find('死亡') != -1:
+                    writedata['dead'] = int(row[pos+1])
+    writedata['patient'] = writedata['hospitalize'] + writedata['wait'] + \
+        writedata['hotel'] + writedata['home'] + writedata['checkout']
+    return writedata
 
 # ファイルのダウンロード
 domain = 'https://www.pref.okinawa.lg.jp'
@@ -72,68 +137,10 @@ save_file = './pdf/' + find_file
 urllib.request.urlretrieve(download_url, save_file)
 print("PDF downloaded at: pdf/" + find_file)
 
-resize_pdf_oka.resize(save_file, './pdf/resize_oka.pdf')
-dummy_line_oka.output_dummy_TypeB('./component/dummy_line_oka.pdf')
-dummy_line_oka.output_mergePDF('./component/dummy_line_oka.pdf',
-                               './pdf/resize_oka.pdf', './pdf/processed_latest_oka.pdf')
-
-pdf = pdfplumber.open('./pdf/processed_latest_oka.pdf')
-
-for page in pdf.pages:
-
-    bounding_box = (250, 74, 400, 90)
-    page_crop = page.within_bbox(bounding_box)
-    page_crop.to_image(resolution=200).save(
-        "./snapshot/lastupdate_oka.png", format="PNG")
-    writedata['lastupdate'] = convertKanjiDateTime2En(page_crop.extract_text())
-
-    tables = page.extract_tables({
-        "vertical_strategy": "lines",
-        "horizontal_strategy": "lines",
-        "intersection_y_tolerance": 1,
-        "min_words_horizontal": 2,
-    })
-
-    for table in tables:
-        localDf = pd.DataFrame(table)
-
-        pos = -1
-        for index, row in localDf.iterrows():
-            if row[0] != None and row[0].find('入院中') != -1:
-                pos = 0
-                break
-            elif row[1] != None and row[1].find('入院中') != -1:
-                pos = 1
-                break
-
-        if pos == -1:
-            break
-        
-        for index, row in localDf.iterrows():
-            if row[pos] == None:
-                print('none')
-            elif len(row[pos]) == 0:
-                if (index == 2 or index == 3) and row[pos+2] != None and row[pos+2].find('中等') != -1:
-                    writedata['moderate'] = int(row[pos+3])
-            elif row[pos].find('入院中') != -1:
-                writedata['hospitalize'] = int(row[pos+1])
-                writedata['severe'] = int(row[pos+3])
-            elif row[pos].find('調整中') != -1:
-                writedata['wait'] = int(row[pos+1])
-            elif row[pos].find('宿泊') != -1:
-                writedata['hotel'] = int(row[pos+1])
-            elif row[pos].find('自宅') != -1:
-                writedata['home'] = int(row[pos+1])
-            elif row[pos].find('解除確認') != -1:
-                writedata['checkout'] = int(row[pos+1])
-            elif row[pos].find('勧告解除') != -1:
-                writedata['release'] = int(row[pos+1])
-            elif row[pos].find('死亡') != -1:
-                writedata['dead'] = int(row[pos+1])
-
-writedata['patient'] = writedata['hospitalize'] + writedata['wait'] + \
-    writedata['hotel'] + writedata['home'] + writedata['checkout']
-
+writedata = pdf_to_data(get_pdf_typeA(save_file))
+print(writedata)
+if writedata['release'] == 0:
+    writedata = pdf_to_data(get_pdf_typeB(save_file))
 print(writedata)
 
 # 情報の保存
