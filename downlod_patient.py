e8b4dbfe2460295d8b3c856940651b898f8fc190
Tomoyuki UBE <ube@coder.okinawa.jp>
2022-03-01 11:06:49 +0900

    bugfix patients file

--- downlod_patient.py
+++ downlod_patient.py
@@ -48,7 +48,7 @@ def Union(dest,unionList):
     return
 
 def Download_FullRecordes():
-    list = ['youseishaitiran_1-40000.csv']
+    list = ['youseishaitiran_00001-40000.csv']
     filename = Download(list)
     unionList = []
     if filename != None:
